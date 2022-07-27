53898e3c2cd562d28382146e20e2808d145dac69
fightaa <fengdeninan331@sohu.com>
2022-07-27 12:50:57 +0800

    chore:reactive

---
+++ src/reactivity/effect.ts
@@ -0,0 +1,16 @@
+
+class activeEffect {
+
+}
+
+export function effect(){
+    return new activeEffect()
+}
+
+export function track(target,key){
+    
+}
+
+export function trigger(target,key){
+
+}
